195b92ff0cb0a6a0d5526ab5ab537ab18da7faa2
yaroslav.furmanov <yaroslav.furmanov@CH-01>
2024-10-15 16:45:05 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿string message = "";
+message += "Hello";
+message += ",";
+message += " ";
+message += "World!";
+Console.WriteLine(message);
+Console.WriteLine("Nowa\nLinia"); Console.WriteLine("Tab\tulacja"); Console.WriteLine("Alarm\a"); Console.WriteLine("Poj.apostrof\'"); Console.WriteLine("cudzyslow\""); Console.WriteLine("Pusty\0");
