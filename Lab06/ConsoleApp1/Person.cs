aca41a66b65c49664e565ac7725ad667a0ac0e32
YaroslavWSEI <yaroslav.furmanov@microsoft.wsei.edu.pl>
2024-12-03 16:55:27 +0100

    Lab06

---
+++ Lab06/ConsoleApp1/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    // class declaration
+    public class Person
+    {
+       
+        // constructor
+        public Person(string firstName, string lastName, DateTime birthDate, string emailAddress)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.BirthDate = birthDate;
+            this.EmailAddress = emailAddress;
+        }
+
+        // properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string EmailAddress { get; set; }
+        public Person Partner { get; set; }
+        public Animal Pet { get; set; }
+        public Appearance appearance { get; set; }
+
+        // method
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+        public int GetAge()
+        {
+            return (int)(DateTime.Now - this.BirthDate).TotalDays / 365;
+        }
+        public bool IsAdult()
+        {
+            return GetAge() >= 18;
+        }
+        public static void Relate(Person p1,Person p2)
+        {
+            p1.Partner = p2;
+            p2.Partner = p1;
+        }
+        public void AdoptPet(Animal a)
+        {
+            this.Pet = a;
+        }
+    }
+}
---
+++ Lab06/ConsoleApp1/Program.cs
@@ -0,0 +1,105 @@
+﻿using ConsoleApp1;
+
+Coords point1 = new Coords(1.0, 1.0);
+Console.WriteLine(point1);
+
+Coords point2 = new Coords(1.0, 1.0); ;
+
+if (point1.Equals(point2))
+    Console.WriteLine("Equal structs");
+else
+    Console.WriteLine("Different structs");
+
+point1 = new Coords(1.0, 1.0);
+point2 = new Coords(2.0, 2.0);
+Console.WriteLine($"Line start=({point1.X},{point1.Y})  Line end=({point2.X},{point2.Y})");
+
+/* Zadanie 1 */
+Console.WriteLine(LenghtOfTheLine());
+
+static double LenghtOfTheLine()
+{ 
+    Coords point1 = new Coords(1.0, 1.0);
+    Coords point2 = new Coords(4.0, 1.0);
+    Coords point3 = new Coords(4.0, 5.0);
+    Coords point4 = new Coords(1.0, 5.0);
+    Console.WriteLine($"Początek linii=({point1})  Koniec linii =({point4}).");
+    Coords[] points = { point1, point2, point3, point4 };
+    double length = GetDistance(points);
+    Console.WriteLine($"Długość linii łamanej: {Math.Round(length, 2)}");
+    return GetDistance(points);
+}
+
+static double GetDistance(Coords[] points)
+{
+    double distance = 0;
+    for (int i = 0; i < points.Length - 1; i++)
+    {
+        Coords a = points[i];
+        Coords b = points[i + 1];
+        distance += Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
+    }
+    return distance;
+}
+
+/* Koniec zadanie 1 */
+
+/* zadanie 2 */
+static (int Min, int Max) GetMinMax(int[] numbers)
+{
+    int min = 0, max = 0;
+    //Opcja 1
+    min = numbers.Min();
+    max = numbers.Max();
+    //Opcja 2
+    foreach (int num in numbers)
+    {
+        if (num < min) min = num;
+        if (num > max) max = num;
+    }
+    return (min, max);
+
+}
+int[] numbers = { 3, 1, 7, 9, -2, 5, 8 };
+var result = GetMinMax(numbers);
+
+Console.WriteLine($"Min: {result.Min}, Max: {result.Max}");
+Console.WriteLine("");
+/* koniec zadanie 2 */
+
+/* Zadanie 3 */
+Person person1 = new Person("John", "Doe", new DateTime(2000, 01, 01), "hhhsddffdgf@microsoft.edu.pl");
+person1.FirstName = "John";
+person1.LastName = "Doe";
+person1.BirthDate = new DateTime(2000, 01, 01);
+person1.EmailAddress = "hhhsddffdgf@microsoft.edu.pl";
+Console.WriteLine($"Person number one = {person1.FirstName} {person1.LastName}. Age: {person1.GetAge()} years old. Email: {person1.EmailAddress} .");
+Person person2 = new Person("Bob", "Bob", new DateTime(2008, 02, 04), "bbbb@bob.com");
+if (person1.IsAdult())
+{
+    Console.WriteLine(" Person one is already an adult ");
+}
+else
+{
+    Console.WriteLine(" Person one  is just a child!");
+}
+person2.FirstName = "Bob";
+person2.LastName = "Bob";
+person2.BirthDate = new DateTime(2008, 02, 04);
+person2.EmailAddress = "bbbb@bob.com";
+Console.WriteLine($"Person number two = {person2.FirstName} {person2.LastName}. Age: {person2.GetAge()} years old. Email: {person2.EmailAddress} .");
+if (person2.IsAdult())
+{
+    Console.WriteLine(" Person two is already an adult.");
+}
+else
+{
+    Console.WriteLine(" Person two is just a child!");
+}
+/* koniec zadanie 3 */
+person1.Partner = person2;
+person2.Partner = person1;
+Person.Relate(person1,person2);
+Animal animal = new Animal() { Species =  "Horse",Name = "Wild Horse"} ;
+person1.Pet = new Animal() { Species = "Cat", Name = "Puszek" };
+person1.AdoptPet(new Animal() { Species = "Cat", Name = "bob" });
