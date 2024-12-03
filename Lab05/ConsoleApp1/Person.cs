09050faddcc6a9ffecccec112f7aa7998e041dac
YaroslavWSEI <yaroslav.furmanov@microsoft.wsei.edu.pl>
2024-12-03 16:34:31 +0100

    Lab05

---
+++ Lab05/ConsoleApp1/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    // class declaration
+    public class Person
+    {
+       
+        // constructor
+        public Person(string firstName, string lastName, DateTime birthDate, string emailAddress)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.BirthDate = birthDate;
+            this.EmailAddress = emailAddress;
+        }
+
+        // properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string EmailAddress { get; set; }
+
+        // method
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+        public int GetAge()
+        {
+            return (int)(DateTime.Now - this.BirthDate).TotalDays / 365;
+        }
+        public bool IsAdult()
+        {
+            return GetAge() >= 18;
+        }
+    }
+}
---
+++ Lab05/ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp1;
+
+Coords point1 = new Coords(2.0, 5.0);
+Console.WriteLine(point1);
+
+Coords point2 = new Coords(3.0, 2.0); ;
+
+if (point1.Equals(point2))
+    Console.WriteLine("Equal structs");
+else
+    Console.WriteLine("Different structs");
+
+//zadanie1
+Console.WriteLine(LenghtOfTheLine());
+static double LenghtOfTheLine()
+{
+    Coords point3 = new Coords(2.0, 5.0);
+    Coords point4 = new Coords(3.0, 2.0);
+    return Math.Sqrt((point3.X - point4.X) * (point3.X - point4.X) + (point3.Y - point4.Y) * (point3.Y - point4.Y));
+}
+
+ 
+static (int Min, int Max) FindMinMax(int[] numbers)
+{
+    if (numbers == null || numbers.Length == 0)
+        Console.WriteLine("Tablica nie może być pusta.");
+
+    int min = numbers[0], max = numbers[0];
+    foreach (var num in numbers)
+    {
+        if (num < min) min = num;
+        if (num > max) max = num;
+    }
+
+    return (min, max);
+}
+Console.WriteLine(FindMinMax);
